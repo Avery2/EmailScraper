b1cfff2c4bad2fb7372df202004eda9aee125e80
Avery Chan <JustinAveryChan@gmail.com>
2021-06-20 17:00:11 -0500

    working search but markeed as bot rn

--- main.py
+++ main.py
@@ -1,75 +1,94 @@
 # import os
 # from googleapi import google
 
-# print("hello world")
-
-# num_page = 3
-# search_results = google.search("This is my query", num_page)
-
-# print(type(search_results))
-# print(search_results)
-
-# google.calculate("157.3kg in grams")
-
-
 # Import the beautifulsoup
 # and request libraries of python.
 import csv
 import requests
 import bs4
 import re
-
-# Make two strings with default google search URL
-# 'https://google.com/search?q=' and
-# our customized search keyword.
-# Concatenate them
-text = "Iain	Donnison 'Aberystwyth University' UK email"
-url = 'https://google.com/search?q=' + text
-
-# Fetch the URL data using requests.get(url),
-# store it in a variable, request_result.
-request_result = requests.get(url)
-
-# Creating soup from the fetched request
-soup = bs4.BeautifulSoup(request_result.text,
-                         "html.parser")
-# print(soup)
-# print(type(soup))
-# print(soup.prettify)
-# print(type(soup.get_text()))
-
-# print(soup.get_text())
+import time
 
 
-# soup.find.all( h3 ) to grab
-# all major headings of our search result,
-heading_object = soup.find_all('h3')
+def findemail(text):
+    # text = "Iain	Donnison 'Aberystwyth University' UK email"
+    url = 'https://google.com/search?q=' + text
+    request_result = requests.get(url)
+    soup = bs4.BeautifulSoup(request_result.text,
+                             "html.parser")
+    # print(soup.get_text())
 
-# Iterate through the object
-# and print it as a string.
-# for info in heading_object:
-#     print(info.getText())
-#     print("------")
+    # soup.find.all( h3 ) to grab
+    # all major headings of our search result,
+    # heading_object = soup.find_all('h3')
 
-txt = soup.get_text()
-# x = re.findall("isd@aber.ac.uk", txt)
-# print(x)
+    # Iterate through the object
+    # and print it as a string.
+    # for info in heading_object:
+    #     print(info.getText())
+    #     print("------")
 
-print()
-print("---"*10)
-print()
+    txt = soup.get_text()
+    # x = re.findall("isd@aber.ac.uk", txt)
+    # print(x)
+    # print(txt)
 
-# x = re.findall("\s.+@.+\..+\s", txt)
-x = re.findall("[^\s]+@[^\s]+\.[^\s]+", txt)
-# print(x)
-# for e in x:
-#     print(e)
-#     print()
+    # print()
+    # print("---"*10)
+    # print()
 
+    # x = re.findall("\s.+@.+\..+\s", txt)
+    x = re.findall("[^\s]+@[^\s]+\.[^\s]+", txt)
+    # print(x)
+    # for e in x:
+    #     print(e)
+    #     print()
+    return x
+
+
+f = open("demofile2.txt", "a")
+num_tries = 2
+try_num = 0
+
+row_quote = 2
 with open('TestCaseEmailScript.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     # print(spamreader)
     for row in spamreader:
-        # print(', '.join(row))
-        if any(row):
+        time.sleep(1.5)
+
+        foo = ' '.join(row).replace(',', ' ').strip()
+        if foo != '':
+            for i, elem in enumerate(row):
+                row[i] = elem.replace(',', ' ')
+            # here no blanks
+            # print(row[0] + " " + row[1])
+            my_str = ''
+            for i, elem in enumerate(row):
+                if i == row_quote:
+                    my_str += "\"" + elem + "\""
+                else:
+                    my_str += " " + elem
+            my_str += " email"
+            print("---" * 10)
             print(row)
+            print(my_str)
+            print(findemail(my_str))
+            print("---" * 10)
+            print()
+            try_num += 1
+            if try_num >= num_tries:
+                break
+
+
+# foo = ''
+# for word in row:
+#     foo += " " + word.replace(',', ' ')
+# if foo.strip():
+#     print(foo)
+#     # print(findemail(foo))
+#     # print()
+#     print("---"*10)
+#     # print()
+# print(row.split(""))
+# print(', '.join(row))
