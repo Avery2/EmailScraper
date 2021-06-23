bfaa183379ed048e25595a2949f7aa6012f49a46
Avery Chan <JustinAveryChan@gmail.com>
2021-06-22 21:24:21 -0500

    comments

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ import time
 from validate_email import validate_email
 
 # ==== CHANGE THESE AS NEEDED ====
-# These are only for testing
-num_tries = 0  # the number of google searches it will do, if 0, will go forever
-doBingSearch = True
-doGoogleSearch = True
-
-input_csv = 'input/TestCaseEmailScript.csv'
-row_quote = 2  # Selects column/term to quote. The columns correspond to the columns of the input csv. (zero indexed)
-
+# the number of google searches it will do, if 0, will go forever
+NUM_SEARCH = 0
+DO_BING_SEARCH = True
+DO_GOOGLE_SEARCH = True
+INPUT_CSV = 'input/TestCaseEmailScript.csv'
+# Selects column/term to quote. The columns correspond to the columns of the input csv. (zero indexed)
+ROW_QUOTE = 2
 # delay between searches (to hopefully avoid bot)
-delaySeconds = 0.01
+DELAY_SECONDS = 0.01
 # row to start from
-rowStart = 1
+START_ROW = 1
 
 # ==== PROBABLY LEAVE ALONE ====
+# Run additional checks on emails
+SECONDARY_EMAIL_CHECK = True
+SLOW_EMAIL_CHECK = False  # this doesn't actually work rn
 
-runSecondaryEmailCheck = True
-runSlowEmailCheck = False  # this doesn't actually work rn
-
-# Initialize values
-try_num = 0
+# ==== INITIALIZE VALUES ====
+queryNum = 0
 GOOGLE_QUERY_URL = 'https://google.com/search?q='
 BING_QUERY_URL = 'https://bing.com/search?q='
 GOOGLE_BOT_MESSAGE = "This page checks to see if it's really you sending the requests, and not a robot."
@@ -48,9 +47,9 @@ def getQueryText(text="default text", queryurl=BING_QUERY_URL):
 
 def findemail(text="default text"):
     txt = ''
-    if doBingSearch:
+    if DO_BING_SEARCH:
         txt += getQueryText(text, BING_QUERY_URL)
-    if doGoogleSearch:
+    if DO_GOOGLE_SEARCH:
         txt += getQueryText(text, GOOGLE_QUERY_URL)
     if len(re.findall(GOOGLE_BOT_MESSAGE, txt)) > 0:
         print("\nWARNING: You may have been flagged as a bot (by Google). Uncomment `print(txt)` in the findemail() function to check.\n")
@@ -65,7 +64,7 @@ def rowToStr(row):
     for i, elem in enumerate(row):
         row[i] = elem.replace(',', ' ')
     for i, elem in enumerate(row):
-        if i == row_quote:
+        if i == ROW_QUOTE:
             my_str += " \"" + elem + "\""
         else:
             my_str += " " + elem
@@ -78,10 +77,10 @@ def filterFoundTerms(foundTerms):
     for mail in foundTerms:
         is_valid_address = validate_email(email_address=mail, check_format=True, check_blacklist=False, check_dns=False, dns_timeout=10, check_smtp=False, smtp_timeout=10, smtp_helo_host=None, smtp_from_address=None, smtp_debug=False)
         print(f"{is_valid_address} : {mail}")
-        if runSecondaryEmailCheck:
+        if SECONDARY_EMAIL_CHECK:
             if is_valid_address:
                 foundTermsFiltered.append(mail)
-            if runSlowEmailCheck:
+            if SLOW_EMAIL_CHECK:
                 is_valid = validate_email(email_address=mail, check_format=True, check_blacklist=True, check_dns=False, dns_timeout=10, check_smtp=True, smtp_timeout=10, smtp_helo_host=None, smtp_from_address=None, smtp_debug=True)
                 print(f"{is_valid} : {mail}")
         else:
@@ -94,19 +93,19 @@ def filterFoundTerms(foundTerms):
 current_time = time.strftime("h%H-m%M-s%S", time.localtime())
 f = open(f"output/foundemails-{current_time}.csv", "a")
 
-# output: do search and write to a csv
-with open(input_csv, newline='') as csvfile:
+# csv output: do search and write to a csv
+with open(INPUT_CSV, newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     # iterate through rows of CSV
     for rowIndex, row in enumerate(spamreader):
-        if rowIndex < rowStart:
+        if rowIndex < START_ROW:
             f.write(f"\n")
             continue
-        time.sleep(delaySeconds)
+        time.sleep(DELAY_SECONDS)
         isNotEmptyRow = ' '.join(row).replace(',', ' ').strip() != ''
         if isNotEmptyRow:
             searchTerm = rowToStr(row)
-            print(f"{try_num} " + "---" * 9)
+            print(f"{queryNum} " + "---" * 9)
             print(f"query list: {row}")
             print(f"search query: {searchTerm}")
             foundTerms = findemail(searchTerm)
@@ -115,14 +114,14 @@ with open(input_csv, newline='') as csvfile:
             print(f"valid emails: {foundTerms}")
             f.write(f"{', '.join(foundTerms)}\n")
             print("---" * 10 + "\n")
-            try_num += 1
-            if num_tries == 0:
+            queryNum += 1
+            if NUM_SEARCH == 0:
                 pass
-            elif try_num >= num_tries:
+            elif queryNum >= NUM_SEARCH:
                 break
 
-# output: write a new combined csv
-with open(input_csv, 'r') as f1, open(f"output/foundemails-{current_time}.csv", 'r') as f2, open(f"output/foundemails-combined-{current_time}.csv", 'w') as w:
+# csv output: write a new combined csv
+with open(INPUT_CSV, 'r') as f1, open(f"output/foundemails-{current_time}.csv", 'r') as f2, open(f"output/foundemails-combined-{current_time}.csv", 'w') as w:
     writer = csv.writer(w)
     r1, r2 = csv.reader(f1), csv.reader(f2)
     while True:
